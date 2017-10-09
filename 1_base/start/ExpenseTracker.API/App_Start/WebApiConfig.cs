5fde71ccf959c2af2835bc3ac83de416b5a3018d
Med <Med@MED-PC>
2017-10-09 17:49:42 +0100

    changes

--- 1_base/start/ExpenseTracker.API/App_Start/WebApiConfig.cs
+++ 1_base/start/ExpenseTracker.API/App_Start/WebApiConfig.cs
@@ -21,15 +21,6 @@ namespace ExpenseTracker.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional });
 
-            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
-
-
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting
-                = Newtonsoft.Json.Formatting.Indented;
-
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver
-                = new CamelCasePropertyNamesContractResolver();
-
             return config;
              
         }
